06badbaa2e99c48f5d566e228918b2c866ac83ea
ssibelcebeci <ssibelcebeci@gmail.com>
2025-11-13 16:52:18 +0400

    add access to Information Video steps and feature

--- src/test/java/stepDefinitions/AccessToInfoVideoSteps.java
+++ src/test/java/stepDefinitions/AccessToInfoVideoSteps.java
@@ -3,29 +3,32 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import pages.EJunkieHomePage;
+import utils.ConfigReader;
 import utils.ReusableMethods;
 
 public class AccessToInfoVideoSteps extends ReusableMethods {
 
+    EJunkieHomePage ejhp=new EJunkieHomePage();
+    Actions actions =new Actions(driver);
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+
     @Given("Navigate to the main webpage")
     public void navigate_to_the_main_webpage() {
-        driver.get("https://www.e-junkie.com/");
+        driver.get(ConfigReader.getProperty("homePageUrl"));
     }
 
     @When("Click the see how it works button")
     public void click_the_see_how_it_works_button() {
-        WebElement howItWorks= driver.findElement(By.xpath("//a[@class='blue_btn']"));
-        howItWorks.click();
+        myClick(ejhp.howItWorksBtn);
     }
 
     @When("Play the information video")
     public void play_the_information_video() {
-        Actions actions =new Actions(driver);
+
         for (int i = 0; i <17 ; i++) {
             actions.sendKeys(Keys.TAB).perform();
         }
@@ -34,7 +37,6 @@ public class AccessToInfoVideoSteps extends ReusableMethods {
 
     @Then("Close the video window")
     public void close_the_video_window() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
         threadWait(10);
         js.executeScript("toggleYoutubeModal(false)");
     }
